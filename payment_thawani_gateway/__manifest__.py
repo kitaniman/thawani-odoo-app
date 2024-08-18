e1b68dae583b53b48a37c99668cccf644d397c2f
kitaniman <abdullahalkitani@gmail.com>
2024-08-18 23:22:49 +0400

    Added link to description

--- payment_thawani_gateway/__manifest__.py
+++ payment_thawani_gateway/__manifest__.py
@@ -5,8 +5,8 @@
     'version': '1.0',
     'category': 'Accounting/Payment Providers',
     'sequence': 350,
-    'summary': "A payment provider based in Oman covering most Omani payment methods.",
-    'description': "An application that adds support for Thawani payment gateway to Odoo.",
+    'summary': "An application that adds support for Thawani payment gateway to Odoo. More information can be found in https://github.com/kitaniman/thawani-odoo-app/tree/main/payment_thawani_gateway",
+    'description': "An application that adds support for Thawani payment gateway to Odoo. More information can be found in https://github.com/kitaniman/thawani-odoo-app/tree/main/payment_thawani_gateway",
     'depends': ['payment'],
     'data': [
         'views/payment_thawani_templates.xml',
